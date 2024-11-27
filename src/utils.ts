3cf451c4ebd22632f3dfc2715e66dd52a2604d5d
Sebastian <sebastian@stryczek.pl>
2024-11-27 18:56:59 +0100

    Update 3D layer to sdk 3

---
+++ src/utils.ts
@@ -0,0 +1,36 @@
+import { Quaternion, Vector3, Matrix4, type Object3D, type PointLight, MathUtils } from "three";
+import { SourceOrientation } from "./types";
+
+const isPointLight = (object3D: Object3D): object3D is PointLight => {
+  return "isPointLight" in object3D && object3D.isPointLight === true;
+};
+
+const sourceOrientationToQuaternion = (so: SourceOrientation | undefined): Quaternion => {
+  const quaternion = new Quaternion();
+
+  switch (so) {
+    case SourceOrientation.X_UP:
+      quaternion.setFromAxisAngle(new Vector3(0, 0, 1), -Math.PI / 2);
+      break;
+
+    case SourceOrientation.Y_UP:
+      quaternion.identity();
+      break;
+
+    case SourceOrientation.Z_UP:
+      quaternion.setFromAxisAngle(new Vector3(1, 0, 0), -Math.PI / 2);
+      break;
+  }
+
+  return quaternion;
+};
+
+const getTransformationMatrix = (scale: number, heading: number, orientation: SourceOrientation): Matrix4 => {
+  const scaleMatrix = new Matrix4().makeScale(scale, scale, scale);
+  const orientationMatrix = new Matrix4().makeRotationFromQuaternion(sourceOrientationToQuaternion(orientation));
+  const headingMatrix = new Matrix4().makeRotationY(MathUtils.degToRad(-heading));
+
+  return new Matrix4().multiply(scaleMatrix).multiply(orientationMatrix).multiply(headingMatrix);
+};
+
+export { isPointLight, getTransformationMatrix };
