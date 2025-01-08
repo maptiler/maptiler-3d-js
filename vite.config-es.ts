622f1f2fe5ec89f87788343a9bf3948731b56f09
Sebastian Stryczek <sebastian@stryczek.pl>
2025-01-08 11:21:04 +0100

    Fix declarations files generation (#17)

--- vite.config-es.ts
+++ vite.config-es.ts
@@ -4,38 +4,34 @@ import dts from 'vite-plugin-dts';
 
 const isProduction = process.env.NODE_ENV === "production";
 
-const plugins = [
-  dts({insertTypesEntry: true}),
-];
-
 export default defineConfig({
   mode: isProduction ? "production" : "development",
   build: {
     minify: isProduction,
     sourcemap: true,
     lib: {
-      // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/maptiler-3d.ts'),
       name: 'maptiler3d',
-      // the proper extensions will be added
       fileName: (format, entryName) => `${entryName}.js`,
       formats: ['es'],
     },
-    
     rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
+      // make sure to externalize deps that shouldn't be bundled into your library
       external: [
         "three",
         "LRUCache",
         "@maptiler/sdk",
       ],
       output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
+        // Provide global variables to use in the UMD build for externalized deps
         globals: {},
       },
     },
   },
-  plugins,
-})
+  plugins:[
+    dts({
+      insertTypesEntry: true,
+      entryRoot: "src",
+    }),
+  ],
+});
