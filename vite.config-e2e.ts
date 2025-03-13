7e676237f76dd97651f9a3678150bc7de42bec14
Les Moffat <les.moffat@maptiler.com>
2025-03-13 15:15:55 +0000

    chore: code tidy to remove unused files and commented code

--- vite.config-e2e.ts
+++ vite.config-e2e.ts
@@ -1,18 +1,6 @@
 import { defineConfig } from 'vite';
-import esConfig from './vite.config-es';
 
 export default defineConfig({
-    ...esConfig,
-    // publicDir: './',
-    // plugins: [
-      // ...(esConfig.plugins ?? []),
-    //   {
-    //   name: 'url-override',
-    //   /**
-    //    * Changing logged URLs to include the path to the demo directory.
-    //    */
-    // }
-  // ],
   build: {
     rollupOptions: {
       input: {
