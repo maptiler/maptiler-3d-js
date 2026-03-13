2911146915b79342d4bda198dd800460cd37bb1d
Les <iamiamiamles@gmail.com>
2026-03-13 09:56:29 +0000

    RD-1765 Move exports to maptiler-3d.ts file

--- src/config.ts
+++ src/config.ts
@@ -1,13 +1,20 @@
-/**
- * Whether to use debug logs.
- * @private
- */
-export const USE_DEBUG_LOGS: boolean = false;
 
-/**
- * An epsilon to make sure the reference anchor point is not exactly at the center of the viewport, but still very close.
- * This is because ThreeJS light shaders were messed up with reference point in the center.
- * This issue is only happening because we are doing the projection matrix trick, otherwise we wouldn't bother with epsilon
- *
- */
-export const EPSILON = 0.01;
+export type MapTiler3DModuleConfig = {
+  /**
+   * Whether to use debug logs.
+   */
+  USE_DEBUG_LOGS: boolean;
+  /**
+   * An epsilon to make sure the reference anchor point is not exactly at the center of the viewport, but still very close.
+   * This is because ThreeJS light shaders were messed up with reference point in the center.
+   * This issue is only happening because we are doing the projection matrix trick, otherwise we wouldn't bother with epsilon
+   *
+   */
+  EPSILON: number;
+};
+
+export const config: MapTiler3DModuleConfig = {
+  USE_DEBUG_LOGS: false,
+  EPSILON: 0.01,
+};
+
--- src/maptiler-3d.ts
+++ src/maptiler-3d.ts
@@ -1,2 +1,25 @@
 export * from "./Layer3D";
-export * from "./types";
+
+export type { Item3D } from "./Item3D";
+
+export {
+  type GenericObject3DOptions,
+  type Layer3DOptions,
+  type MeshOptions,
+  type AnimationMode,
+  type AnimationLoopOptions,
+  type Item3DTransform,
+  type Item3DMeshUIStateName,
+  type Item3DMeshUIStateProperties,
+  type Item3DMeshUIStates,
+  type Item3DEventTypes,
+  type AddMeshFromURLOptions,
+  type CloneMeshOptions,
+  type PointLightOptions,
+  AltitudeReference,
+  AnimationLoopOptionsMap,
+  SourceOrientation,
+} from "./types";
+
+export { getTransformationMatrix } from "./utils";
+export { type MapTiler3DModuleConfig, config } from "./config";
